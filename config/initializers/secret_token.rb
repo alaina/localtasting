28c3c193f992be8ab9f18f7db120208348aeea5f
Alaina Percival <alainapercival@gmail.com>
2011-12-07 20:38:28 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Localtasting::Application.config.secret_token = '3353cb7bb056312d02d707526d55b6704e5da10d28a06578d0f1d3afe10b73405677d0d165144ef665e69daca4df42a84ca028994a13c3845bc2138887097f82'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Localtasting::Application.config.session_store :cookie_store, :key => '_localtasting_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Localtasting::Application.config.session_store :active_record_store
